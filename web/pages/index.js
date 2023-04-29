10f8d6feac94da68d4779b8a29861dcdbae3386b
CedricMurairi <murairicedric@gmail.com>
2023-04-29 10:56:04 +0200

    home page

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,12 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Spendings from "@pages/spendings";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-center p-24`}
-    >
-      <h1 className="text-4xl font-bold text-center">Expense Tracker</h1>
-      <p className="text-xl text-center mt-4">Coming soon...</p>
+    <main>
+      <Spendings />
     </main>
-  )
+  );
 }
