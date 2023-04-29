9c07c460944ecf4448c6cc24267558dbd06ec751
CedricMurairi <murairicedric@gmail.com>
2023-04-29 11:01:21 +0200

    designed recommendation cards and displayed recommendation data

---
+++ web/components/recommendation/savings_card.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+export default function SavingsRecommendationCard({ data }) {
+  const [showMessages, setShowMessages] = useState(false);
+  return (
+    <div className="flex flex-col bg-red-50 rounded-lg my-3 px-3 py-2">
+      {Object.keys(data).map((entry, index) =>
+        entry === "messages" ? (
+          <div className="list-none" key={index}>
+            {showMessages &&
+              Object.keys(data.messages).map((innerIndex) => {
+                return (
+                  <li className="text-md font-thin" key={innerIndex}>
+                    {data.messages[innerIndex]}
+                  </li>
+                );
+              })}
+          </div>
+        ) : (
+          <div key={index} className="px-2 py-1">
+            {entry.includes("_percent") ? (
+              <div className="flex flex-row justify-between items-center">
+                <p className="text-xl font-[300]">{entry.split("_")[0]}</p>
+                <p
+                  className={`${
+                    entry.includes("_increase")
+                      ? "text-green-600"
+                      : "text-red-600"
+                  } text-2xl font-bold`}
+                >
+                  {data[entry]}
+                  {"%"}
+                </p>
+              </div>
+            ) : (
+              <div
+                key={index}
+                className="flex flex-row justify-between items-center"
+              >
+                <p className="text-xl font-[300]">{entry}</p>
+                <p
+                  className={`${
+                    entry.includes("_increase")
+                      ? "text-green-600"
+                      : "text-orange-400"
+                  } text-2xl font-bold`}
+                >
+                  {"RWF"}
+                  {Math.round(data[entry])}
+                </p>
+              </div>
+            )}
+          </div>
+        )
+      )}
+      <button
+        onClick={() => {
+          setShowMessages(!showMessages);
+        }}
+        className="underline text-xs font-thin"
+      >
+        {showMessages ? "Show less" : "Show more"}
+      </button>
+    </div>
+  );
+}
---
+++ web/pages/recommendation.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import MainLayout from "@components/layout/main_layout";
+import UserData from "@mock/user_one.json";
+import fetch_recommendations from "@data/recomendation";
+import RecommendationCard from "@components/recommendation/card";
+import SavingsRecommendationCard from "@components/recommendation/savings_card";
+
+export default function Recommendation() {
+  const [recommendations, setRecommendations] = useState(null);
+
+  useEffect(() => {
+    fetch_recommendations(UserData)
+      .then((data) => {
+        setRecommendations(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [UserData]);
+
+  return (
+    <MainLayout headerContent={"Recommendation"} page={"Recommendation"}>
+      <div className="grid grid-cols-3 gap-10">
+        {recommendations &&
+          Object.keys(recommendations).map((recommendation, index) =>
+            recommendation === "possible_savings" ||
+            recommendation === "potential_savings" ? (
+              <div key={index} className="overflow-auto">
+                <h1 className="text-2xl underline">
+                  {recommendation.split("_").join(" ").toUpperCase()}
+                </h1>
+                <SavingsRecommendationCard data={recommendations[recommendation]}/>
+              </div>
+            ) : (
+              <div key={index}>
+                <h1 className="text-2xl underline">
+                  {recommendation.toUpperCase()}
+                </h1>
+                {Object.keys(recommendations[recommendation]).map(
+                  (entry, subIndex) => (
+                    <RecommendationCard
+                      header={entry}
+                      key={subIndex}
+                      data={recommendations[recommendation][entry]}
+                    />
+                  )
+                )}
+              </div>
+            )
+          )}
+      </div>
+    </MainLayout>
+  );
+}
