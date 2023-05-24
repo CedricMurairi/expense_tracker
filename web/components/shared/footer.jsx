c7b9162093aefb0ff39ea1256306c308c5b67b6c
CedricMurairi <murairicedric@gmail.com>
2023-05-24 13:12:41 +0200

    polished the login (landing page) and updated the footer

--- web/components/shared/footer.jsx
+++ web/components/shared/footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Footer() {
-    return (
-        <div className="text-xs mt-20 font-extralight">Designed with Brains by Cédric Aidan Swift</div>
-    );
+  return (
+    <div className="text-xs mt-20 font-extralight text-center">
+      <div>Designed with Brains.</div>
+      <div className="flex gap-2">
+        <Link href="term-of-service">Term of Service</Link>
+        <Link href="privacy-policy">Privacy Policy</Link>
+      </div>
+      <p>&copy; 2023 Cédric Murairi</p>
+    </div>
+  );
 }
--- web/pages/login.jsx
+++ web/pages/login.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { continueWithGoogle } from "@data/auth";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "firebaseconfig";
 import { useRouter } from "next/router";
+import { motion } from "framer-motion";
+import Footer from "@components/shared/footer";
 
 export default function Login() {
   const route = useRouter();
@@ -27,14 +29,60 @@ export default function Login() {
     }
   };
 
+  const slider = [
+    "Track your expenses per category",
+    "Set financial goals",
+    "Get recommendations, insights and more",
+    "Optimize your spending habits",
+    "All in one place",
+    "Track Now!",
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === slider.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <button
-        onClick={login}
-        className="py-2 px-5 border border-black rounded-xl"
-      >
-        Continue With Google
-      </button>
+      <div className="flex flex-col justify-center items-center">
+        <div className="text-4xl font-bold mb-5">
+          <span className="text-gray-500">Ex</span>
+          <span className="text-gray-700">Tracker</span>
+        </div>
+        <div>
+          <motion.div
+            className="text-2xl font-bold text-center mb-10 text-gray-500"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            {slider[currentIndex]}
+          </motion.div>
+        </div>
+        <button
+          onClick={login}
+          className="flex items-center py-2 px-5 border border-gray-500 rounded-xl"
+        >
+          <img
+          className="inline-block mr-2"
+            width="25"
+            height="25"
+            src="https://img.icons8.com/color/48/google-logo.png"
+            alt="google-logo"
+          />
+          Continue With Google
+        </button>
+      </div>
+      <Footer />
     </div>
   );
 }
