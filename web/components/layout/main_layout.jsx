e3f19ecbbfda56060543e25ee752a0ea46233aa6
CedricMurairi <murairicedric@gmail.com>
2023-05-26 21:30:07 +0200

    added store to main layout and consumed data in header component

--- web/components/layout/main_layout.jsx
+++ web/components/layout/main_layout.jsx
@@ -1,19 +1,31 @@
 import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Header from "@components/shared/header";
 import Footer from "@components/shared/footer";
 import NavigationBar from "@components/shared/navigation";
 import Head from "next/head";
+import Info from "@components/shared/info";
+import { setUser } from "@store/userSlice";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "firebaseconfig";
 import { useRouter } from "next/router";
 
 export default function MainLayout({ children, headerContent, page }) {
   const route = useRouter();
+  const info = useSelector((state) => state.info.value);
+  const user = useSelector((state) => state.user.value);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        console.log(user.displayName);
+    onAuthStateChanged(auth, (fireUser) => {
+      if (fireUser) {
+        dispatch(setUser({
+          uid: fireUser.uid,
+          name: fireUser.displayName,
+          email: fireUser.email,
+          photo: fireUser.photoURL,
+          accessToken: fireUser.accessToken,
+        }));
       } else {
         route.push("/login");
       }
@@ -24,6 +36,7 @@ export default function MainLayout({ children, headerContent, page }) {
     <div
       className={`relative flex min-h-screen flex-col items-center justify-center p-24`}
     >
+      <Info data={info} />
       <Head>
         <title>{page}</title>
       </Head>
--- web/components/shared/navigation.jsx
+++ web/components/shared/navigation.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { auth } from "firebaseconfig";
+import { useSelector, useDispatch } from "react-redux";
+import { removeUser } from "@store/userSlice";
+import Image from "next/image";
 
 export default function NavigationBar({ page }) {
   const route = useRouter();
+  const user = useSelector((state) => state.user.value);
+  const dispatch = useDispatch();
   let routes = {
     Home: "/",
     Insights: "/insights",
@@ -15,6 +20,7 @@ export default function NavigationBar({ page }) {
   const logout = async () => {
     try {
       await auth.signOut(auth);
+      dispatch(removeUser());
       route.push("/login");
     } catch (e) {
       console.log(e);
@@ -22,17 +28,32 @@ export default function NavigationBar({ page }) {
   };
 
   return (
-    <div className="flex flex-row gap-10">
+    <div className="flex flex-row gap-10 items-center">
       {Object.keys(routes).map((route, index) => {
         return (
-          <li key={index} className={`${page===route ? "underline" : ""} list-none`}>
+          <li
+            key={index}
+            className={`${page === route ? "underline" : ""} list-none`}
+          >
             <Link href={routes[route]}>{route}</Link>
           </li>
         );
       })}
-      <div>
-        <button onClick={logout}>Logout</button>
-      </div>
+      {user ? (
+        <div className="flex flex-row-reverse gap-2 items-center">
+          <Image
+            alt="user photo"
+            className="rounded-full"
+            width={40}
+            height={40}
+            src={user.photo}
+          />
+          <div className="text-right">
+            <p className="font-bold text-sm">{user.name}</p>
+            <p className="text-xs">{user.email}</p>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
