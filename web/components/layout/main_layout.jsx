3616a51e04f848a521b6882d1f41ecd962b7096d
CedricMurairi <murairicedric@gmail.com>
2023-05-28 20:44:23 +0200

    fixed user load on main layout and setup settings

--- web/components/layout/main_layout.jsx
+++ web/components/layout/main_layout.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Header from "@components/shared/header";
 import Footer from "@components/shared/footer";
@@ -11,6 +11,7 @@ import { auth } from "firebaseconfig";
 import { useRouter } from "next/router";
 
 export default function MainLayout({ children, headerContent, page }) {
+  const [showSettings, setShowSettings] = useState(false);
   const route = useRouter();
   const info = useSelector((state) => state.info.value);
   const user = useSelector((state) => state.user.value);
@@ -19,13 +20,15 @@ export default function MainLayout({ children, headerContent, page }) {
   useEffect(() => {
     onAuthStateChanged(auth, (fireUser) => {
       if (fireUser) {
-        dispatch(setUser({
-          uid: fireUser.uid,
-          name: fireUser.displayName,
-          email: fireUser.email,
-          photo: fireUser.photoURL,
-          accessToken: fireUser.accessToken,
-        }));
+        dispatch(
+          setUser({
+            uid: fireUser.uid,
+            name: fireUser.displayName,
+            email: fireUser.email,
+            photo: fireUser.photoURL,
+            accessToken: fireUser.accessToken,
+          })
+        );
       } else {
         route.push("/login");
       }
@@ -33,16 +36,24 @@ export default function MainLayout({ children, headerContent, page }) {
   }, []);
 
   return (
-    <div
-      className={`relative flex min-h-screen flex-col items-center justify-center p-24`}
-    >
+    <div className="relative flex min-h-screen flex-col items-center justify-center p-24">
       <Info data={info} />
       <Head>
         <title>{page}</title>
       </Head>
-      <NavigationBar page={page} />
+      <NavigationBar
+        page={page}
+        showSettings={showSettings}
+        setShowSettings={setShowSettings}
+      />
       <Header content={headerContent} />
-      {children}
+      <div
+        className={`${
+          showSettings ? "blur" : "blur-none"
+        }`}
+      >
+        {children}
+      </div>
       <Footer />
     </div>
   );
--- web/components/shared/navigation.jsx
+++ web/components/shared/navigation.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { auth } from "firebaseconfig";
@@ -6,7 +6,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { removeUser } from "@store/userSlice";
 import Image from "next/image";
 
-export default function NavigationBar({ page }) {
+export default function NavigationBar({ page, showSettings, setShowSettings }) {
   const route = useRouter();
   const user = useSelector((state) => state.user.value);
   const dispatch = useDispatch();
@@ -27,6 +27,10 @@ export default function NavigationBar({ page }) {
     }
   };
 
+  const showSomething = () => {
+    return;
+  }
+
   return (
     <div className="flex flex-row gap-10 items-center">
       {Object.keys(routes).map((route, index) => {
@@ -40,18 +44,31 @@ export default function NavigationBar({ page }) {
         );
       })}
       {user ? (
-        <div className="flex flex-row-reverse gap-2 items-center">
-          <Image
-            alt="user photo"
-            className="rounded-full"
-            width={40}
-            height={40}
-            src={user.photo}
-          />
-          <div className="text-right">
-            <p className="font-bold text-sm">{user.name}</p>
-            <p className="text-xs">{user.email}</p>
+        <div className="relative">
+          <div
+            onClick={() => setShowSettings(!showSettings)}
+            className="cursor-pointer flex flex-row-reverse gap-2 items-center"
+          >
+            <Image
+              alt="user photo"
+              className="rounded-full"
+              width={40}
+              height={40}
+              src={user.photo}
+            />
+            <div className="text-right">
+              <p className="font-bold text-sm">{user.name}</p>
+              <p className="text-xs">{user.email}</p>
+            </div>
           </div>
+          {showSettings ? (
+            <div className="z-50 py-5 px-5 absolute top-14 w-[250px] h-[200px] rounded-lg border-[0.1px] border-gray-200 bg-gray-50 shadow-2xl shadow-slate-300">
+              <h4 className="border-b border-gray-200 pb-1">Settings</h4>
+              <ul className="pt-2">
+                <li onClick={() => showSomething()} className="cursor-pointer flex justify-between w-full border-y border-gray-300"><span>Enter some stuff</span><span>{">"}</span></li>
+              </ul>
+            </div>
+          ) : null}
         </div>
       ) : null}
     </div>
