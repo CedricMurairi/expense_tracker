6844a37030f5b51ae40022bfea775bb93f1099e0
CedricMurairi <murairicedric@gmail.com>
2023-05-30 15:56:39 +0200

    add custom app error handler

--- api/setup.py
+++ api/setup.py
@@ -3,7 +3,7 @@ from app.goals import goals_blueprint
 from app.insights import insights_blueprint
 from app.recommendations import recommendations_blueprint
 from app.expenditures import expenditures_blueprint
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request, g, jsonify
 from flask_cors import CORS
 from firebase import db
 from firebase import app as firebase_app
@@ -22,27 +22,79 @@ app.register_blueprint(payments_blueprint, url_prefix="/payments")
 app.add_url_rule('/', endpoint='home')
 
 
+class InvalidContentTypeError(Exception):
+    pass
+
+
+class NoDataProvidedError(Exception):
+    pass
+
+
+class UnauthorizedClientError(Exception):
+    pass
+
+
 @app.before_request
 def check_user():
-    if request.method == "GET":
-        pass
-    if request.content_type != 'application/json':
-        return {"error": "Invalid Content-Type"}
-    data = request.get_json()
-    if data is None or data == {}:
-        return {"error": "No data provided"}
+    if request.method == "POST":
+        if request.content_type != 'application/json':
+            raise InvalidContentTypeError()
+        data = request.get_json()
+        if not data:
+            raise NoDataProvidedError()
 
-    try:
-        id_token = request.authorization.token
-        decoded_token = auth.verify_id_token(id_token, app=firebase_app)
-        g.token = decoded_token
-    except Exception as e:
-        print(e)
+        try:
+            id_token = request.authorization.token
+            decoded_token = auth.verify_id_token(id_token, app=firebase_app)
+            g.token = decoded_token
+        except Exception as e:
+            raise UnauthorizedClientError()
+
+
+@app.errorhandler(InvalidContentTypeError)
+def handle_invalid_content_type_error(error):
+    response = jsonify({"error": "Invalid Content-Type"})
+    response.status_code = 400
+    return response
+
+
+@app.errorhandler(NoDataProvidedError)
+def handle_no_data_provided_error(error):
+    response = jsonify({"error": "No data provided"})
+    response.status_code = 400
+    return response
+
+
+@app.errorhandler(UnauthorizedClientError)
+def handle_verification_error(error):
+    response = jsonify(
+        {"error": "You are not authorized to access this resource"})
+    response.status_code = 401
+    return response
+
+
+@app.errorhandler(404)
+def handle_not_found_error(error):
+    response = jsonify(
+        {"error": "Could not find the resource you are trying to access"})
+    response.status_code = 404
+    return response
+
+
+@app.errorhandler(500)
+def handle_internal_server_error(error):
+    response = jsonify(
+        {"error": "There is an internal server error, try back later"})
+    response.status_code = 500
+    return response
 
 
 @app.route("/")
 def home():
-    return {"message": "API up", "action": "Go to /docs for documentation"}, 200
+    response = {"message": "API up",
+                "action": "Go to /docs for documentation"}, 200
+    response.status_code = 200
+    return response
 
 
 @app.route("/docs")
