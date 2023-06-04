16ecb0d92ba5364e19bee3076c1879ab46965341
CedricMurairi <murairicedric@gmail.com>
2023-06-04 13:09:06 +0200

    moved to RTK and used proper loading state in components

--- web/pages/spendings.jsx
+++ web/pages/spendings.jsx
@@ -1,32 +1,44 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import MainLayout from "@components/layout/main_layout";
 import Form from "@components/spending/form";
 import { useDispatch, useSelector } from "react-redux";
-import getData from "@helpers/get_data";
-import { listenForData } from "@helpers/get_data";
+import { getMonthlySpendings } from "@helpers/data";
+import { setData } from "@store/dataSlice";
+import { useGetExpendituresQuery } from "@data/base_api";
+import Pulser from "@components/shared/pulser";
 
 export default function Spendings() {
   const dispatch = useDispatch();
-  const data = useSelector((state) => state.data);
+  const stateData = useSelector((state) => state.data);
+  const user = useSelector((state) => state.user.value);
+  const { data, currentData, isLoading, isError } = useGetExpendituresQuery();
 
   useEffect(() => {
-    getData(dispatch);
-    return listenForData(dispatch);
-  }, []);
+    if (data) {
+      const monthlySpendings = getMonthlySpendings(data.expenditures);
+      dispatch(setData({ expenditures: data.expenditures, monthlySpendings }));
+    }
+  }, [data]);
 
   return (
     <MainLayout headerContent="Spendings" page="Home">
       <div className="w-full">
         <div className="max-sm:text-[15px] max-sm:mb-3 text-xl flex flex-row justify-center items-center mb-5 font-bold">
           <p className=" bold py-1 text-green-500">
-            In: {data.value?.income} RWF
+            In: {stateData.value?.income} RWF
           </p>
           <span className="mx-2 text-gray-500">{"|"}</span>
-          <p className=" bold py-1 text-orange-500">
-            Out: {data.value?.monthlySpendings} RWF
-          </p>
+          {isLoading ? (
+            <span className="px-5">
+              <Pulser primary={"bg-slate-500"} secondary={"bg-slate-400"}/>
+            </span>
+          ) : (
+            <p className=" bold py-1 text-orange-500">
+              Out: {stateData.value?.monthlySpendings} RWF
+            </p>
+          )}
         </div>
-        <Form />
+        <Form/>
       </div>
     </MainLayout>
   );
