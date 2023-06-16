863bd9ffabafac57637fcc41c1350bc3164f3ed2
CedricMurairi <murairicedric@gmail.com>
2023-06-16 11:52:04 +0200

    accounts for null values and zero -- does not apply for less than 10000

--- web/helpers/format_number.js
+++ web/helpers/format_number.js
@@ -1,5 +1,6 @@
 export default function formatNumber(num) {
-  if (num >= 1000 && num < 1000000) {
+  if (!num || num === 0) return 0;
+  if (num >= 10000 && num < 1000000) {
     const formatted = (num / 1000).toFixed(1);
     return formatted.replace(/\.0$/, "") + "K";
   } else if (num >= 1000000) {
