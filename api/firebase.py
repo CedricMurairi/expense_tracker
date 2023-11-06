5a3d0d48172fdf014add2f319ad4e7b436711498
CedricMurairi <murairicedric@gmail.com>
2023-11-06 18:34:44 +0200

    used environment variable with os instead of self-discovery

--- api/firebase.py
+++ api/firebase.py
@@ -1,5 +1,8 @@
+import os
 import firebase_admin
-from firebase_admin import firestore
+from firebase_admin import firestore, credentials
 
-app = firebase_admin.initialize_app()
+cred = credentials.Certificate(
+    os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"))
+app = firebase_admin.initialize_app(cred)
 db = firestore.client(app)
